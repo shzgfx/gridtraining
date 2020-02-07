25fde65e093e64efcca93988d823ac370a87b5de
Tomasz Kępa <tomaszkepa10@gmail.com>
2020-02-07 18:43:20 +0100

    added responsive and hamburger menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const navigationSwitcher = document.querySelector(".nav__switcher--js");
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationMenu = document.querySelector(".nav__menu--js")
+    navigationMenu.classList.toggle("nav__menu--visible");
+        
+    } 
+)
